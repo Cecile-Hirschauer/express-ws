f92adca7b33124408f41837c519e8019a3f2200f
Cécile HIRSCHAUER <cecile.hirschauer@suricats-consulting.com>
2022-03-16 14:45:05 +0100

    First test: Sign Up Request

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,5 +1,15 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(() => {
+  sequelize.sync();
+});
+
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe('User Registration', () => {
   it('returns 200 OK when signup request is valid', (done) => {
@@ -27,6 +37,40 @@ describe('User Registration', () => {
       .then((response) => {
         expect(response.body.message).toBe('User created');
         done();
+      });
+  });
+
+  it('saves the user in the database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user6',
+        email: 'user6@email.com',
+        password: 'password',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it('saves username and email in database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user6',
+        email: 'user6@email.com',
+        password: 'password',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          const userSaved = userList[0];
+          expect(userSaved.username).toBe('user6');
+          expect(userSaved.email).toBe('user6@email.com');
+          done();
+        });
       });
   });
 });
